8bcc9ec0f28e64df293ab85e42dd18315ecb2af8
Jesse Govindan <jesse.govindan@chillisoft.co.za>
2025-03-15 15:26:11 +0200

    Flesh out exercise-2.spec.ts

--- src/test/exercise-2.spec.ts
+++ src/test/exercise-2.spec.ts
@@ -1,11 +1,12 @@
 import { applyDiscountCode } from '../exercises/exercise-2'
 import * as cartDb from '../common/CartDb'
 import { randomBytes } from 'crypto'
-import { Ok } from '../common/Response'
+import { BadRequest, Ok } from '../common/Response'
 
 describe('applyDiscountCode', () => {
   const getCartMock = vi.spyOn(cartDb, 'getUserCart')
   const getDiscountForCodeMock = vi.spyOn(cartDb, 'getDiscountForCode')
+  const updateUserCartMock = vi.spyOn(cartDb, 'updateUserCart')
 
   it("returns the user's cart with the discount applied to every item", () => {
     // Arrange
@@ -14,7 +15,7 @@ describe('applyDiscountCode', () => {
     const discount = 0.23
     getDiscountForCodeMock.mockReturnValue(discount)
     // Act
-    const response = sut(randomString())
+    const response = sut()
     // Assert
     expect(response).toEqual(Ok({
       body: [
@@ -30,7 +31,48 @@ describe('applyDiscountCode', () => {
     }))
   })
 
-  function sut(discountCode: string, userId: string = randomString()) {
+  it(`updates the user's cart with discount applied to every item`, () => {
+    // Arrange
+    const userCart = [createCartItem({ discount: 0 }), createCartItem({ discount: 0 })] 
+    getCartMock.mockReturnValue(userCart)
+    const discount = 0.23
+    getDiscountForCodeMock.mockReturnValue(discount)
+    const userId = randomString()
+    // Act
+    sut({ userId })
+    // Assert
+    expect(updateUserCartMock).toHaveBeenCalledWith(userId, [
+      {
+        ...userCart[0],
+        discount,
+      },
+      {
+        ...userCart[1],
+        discount,
+      },
+    ])
+  })
+
+  it('returns a BadRequest when body of request is not a string', () => {
+    // Act
+    const result = sut({ discountCode: 123 })
+    // Assert
+    expect(result).toEqual(BadRequest({ body: 'Unable to apply discount code. Invalid discount code provided.' }))
+  })
+
+  it('returns a BadRequest when discount code is not valid', () => {
+    // Arrange
+    getDiscountForCodeMock.mockReturnValue(0)
+    // Act
+    const result = sut({ discountCode: randomString() })
+    // Assert
+    expect(result).toEqual(BadRequest({
+      body: 'Unable to apply discount code. No discount found for the given discount code.'
+    }))
+  })
+
+  function sut(params: { discountCode?: any; userId?: string } = {}) {
+    const { discountCode = randomString(), userId = randomString() } = params
     return applyDiscountCode({ body: discountCode, session: { userId } })
   }
 
