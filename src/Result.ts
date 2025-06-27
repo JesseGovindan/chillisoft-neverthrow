9947b90a401bc048cfc741724bb1f49910df62c3
Moheshen Chetty <moheshen.chetty@chillisoft.co.za>
2025-06-27 14:17:19 +0200

    Exercise 1 complete

--- src/Result.ts
+++ src/Result.ts
@@ -1,16 +1,20 @@
 // Make whatever changes this to class to help you implement the functionality required
 export class Result<V, E> {
-  private constructor(private isOk: boolean, private value?: V, private error?: E) {
-  }
+  private constructor(
+    private isOk: boolean,
+    private value?: V,
+    private error?: E
+  ) {}
 
   static ok<V, E>(value: V) {
-    return new Result<V, E>(true, value, undefined)
+    return new Result<V, E>(true, value, undefined);
   }
 
   static err<V, E>(error: E) {
-    return new Result<V, E>(false, undefined, error)
+    return new Result<V, E>(false, undefined, error);
   }
 
   match<R>(handleValue: (value: V) => R, handleError: (error: E) => R): R {
+    return this.isOk ? handleValue(this.value!) : handleError(this.error!);
   }
 }
--- src/test/exercise-1.spec.ts
+++ src/test/exercise-1.spec.ts
@@ -1,76 +1,76 @@
-import { UserTemplate } from '../common/UserDb'
-import { addUser } from '../exercises/exercise-1'
+import { UserTemplate } from "../common/UserDb";
+import { addUser } from "../exercises/exercise-1";
 
-describe('addUser', () => {
+describe("addUser", () => {
   function createValidUser() {
     return {
-      name: 'John Doe',
-      email: 'john.doe@yahoo.com',
-      password: 'DoeADeerAFemailDeer',
-    }
+      name: "John Doe",
+      email: "john.doe@yahoo.com",
+      password: "DoeADeerAFemailDeer",
+    };
   }
 
   function createValidUserWithout<T extends keyof UserTemplate>(key: T) {
-    const user: any = createValidUser()
-    delete user[key]
-    return user
+    const user: any = createValidUser();
+    delete user[key];
+    return user;
   }
 
-  it('returns status 201 when all user field provided in request', () => {
+  it("returns status 201 when all user field provided in request", () => {
     // Arrange
-    const user = createValidUser()
+    const user = createValidUser();
     // Act
-    const response = addUser({ body: user })
+    const response = addUser({ body: user });
     // Assert
-    expect(response.statusCode).toEqual(201)
-  })
+    expect(response.statusCode).toEqual(201);
+  });
 
-  it('returns status 400 when request body is not an object', () => {
+  it("returns status 400 when request body is not an object", () => {
     // Act
-    const response = addUser({ body: '' })
+    const response = addUser({ body: "" });
     // Assert
     expect(response).toEqual({
       statusCode: 400,
-      body: 'Unable to create new user. Body of request is not an object.',
-    })
-  })
+      body: "Unable to create new user. Body of request is not an object.",
+    });
+  });
 
   // Remove this skip when working on exercise-1
-  describe.skip('extra validation', () => {
-    it('returns status 400 when request body is missing email', () => {
+  describe("extra validation", () => {
+    it("returns status 400 when request body is missing email", () => {
       // Arrange
-      const user: any = createValidUserWithout('email')
+      const user: any = createValidUserWithout("email");
       // Act
-      const response = addUser({ body: user })
+      const response = addUser({ body: user });
       // Assert
       expect(response).toEqual({
         statusCode: 400,
-        body: 'Unable to create new user. No email provided.',
-      })
-    })
+        body: "Unable to create new user. No email provided.",
+      });
+    });
 
-    it('returns status 400 when request body is missing password', () => {
+    it("returns status 400 when request body is missing password", () => {
       // Arrange
-      const user: any = createValidUserWithout('password')
+      const user: any = createValidUserWithout("password");
       // Act
-      const response = addUser({ body: user })
+      const response = addUser({ body: user });
       // Assert
       expect(response).toEqual({
         statusCode: 400,
-        body: 'Unable to create new user. No password provided.',
-      })
-    })
+        body: "Unable to create new user. No password provided.",
+      });
+    });
 
-    it('returns status 400 when request body is missing name', () => {
+    it("returns status 400 when request body is missing name", () => {
       // Arrange
-      const user: any = createValidUserWithout('name')
+      const user: any = createValidUserWithout("name");
       // Act
-      const response = addUser({ body: user })
+      const response = addUser({ body: user });
       // Assert
       expect(response).toEqual({
         statusCode: 400,
-        body: 'Unable to create new user. No name provided.',
-      })
-    })
-  })
-})
+        body: "Unable to create new user. No name provided.",
+      });
+    });
+  });
+});
