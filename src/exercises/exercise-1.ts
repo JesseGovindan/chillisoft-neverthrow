0e97ccff8e9dcfa6ba9d6df788f3446ca617d18a
Thunyiwe Mkhize <thunyiwe.mkhize@chillisoft.com>
2025-07-11 15:19:34 +0200

    work on completing the first exercise

--- src/exercises/exercise-1.ts
+++ src/exercises/exercise-1.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "../common/RequestHandler";
 import { BadRequest, Created } from "../common/Response";
 import { createUser } from "../common/UserDb";
+import { Result } from "../Result";
 
 // Refactor this function so that it returns a better error responses.
 // i.e 'Unable to create user. Invalid email address'
@@ -9,47 +10,62 @@ import { createUser } from "../common/UserDb";
 // hint: You refactored code should not need a try-catch or a check if the userTemplate is defined :)
 // hint: Enable the "extra validation" tests in the exercise-1.spec.ts to verify your work
 export const addUser: RequestHandler = (request) => {
-  try {
-    const userTemplate = validateUserTemplate(request.body)
+  return validateUserTemplate(request.body).match(
+    (response) => {
+      createUser(response!);
+      return Created();
+    },
+    (error) => {
+      const errorMessage =
+        request.body === ""
+          ? "Unable to create new user. Body of request is not an object."
+          : error === "Invalid string" ?  "Unable to create new user. Invalid user details provided.": error;
 
-    if (!userTemplate) {
-      return BadRequest({ body: 'Unable to create new user. Invalid user details provided.' })
+      return BadRequest({ body: errorMessage });
     }
+  );
+};
 
-    createUser(userTemplate)
-
-    return Created()
-  } catch {
-    return BadRequest({ body: 'Unable to create new user. Body of request is not an object.' })
-  }
-}
-
-function validateUserTemplate(possibleUser: any) {
-  if (typeof possibleUser !== 'object') {
-    throw new Error('Invalid body')
+const validateUserTemplate = (
+  possibleUser: any
+): Result<{ name: string; email: string; password: string }, string> => {
+  if (typeof possibleUser !== "object") {
+    return Result.err("Invalid body");
   }
 
-  const { email, name, password } = possibleUser
+  const { email, name, password } = possibleUser;
+
+  if(!email) {
+    return Result.err('Unable to create new user. No email provided.')
+  }
+
+  if(!name){
+    return Result.err('Unable to create new user. No name provided.')
+  }
+
+  if(!password){
+    return Result.err('Unable to create new user. No password provided.')
+  }
 
   if (!isAValidString(email)) {
-    return undefined
+    return Result.err("Invalid string");
   }
 
   if (!isAValidString(name)) {
-    return undefined
+    return Result.err("Invalid string");
   }
 
   if (!isAValidString(password)) {
-    return undefined
+    return Result.err("Invalid string");
   }
 
-  return {
+  return Result.ok({
     email,
     name,
     password,
-  }
-}
+  });
+};
 
-function isAValidString(value: any): value is string {
-  return typeof value === 'string' && !!value
+const isAValidString = (value: any): value is string => {
+  return typeof value === "string" && !!value;
 }
