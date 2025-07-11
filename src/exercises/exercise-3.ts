3076b4e7f736c5e8dadfaaf326a23c1960e2ea6c
Moheshen Chetty <moheshen.chetty@chillisoft.co.za>
2025-07-11 10:28:40 +0200

    Refactored getWeatherSummary - finished Exercise 3

--- src/exercises/exercise-3.ts
+++ src/exercises/exercise-3.ts
@@ -1,6 +1,11 @@
 import { RequestHandler } from "../common/RequestHandler";
 import { BadRequest, Ok } from "../common/Response";
-import { fetchWeatherCondition, WeatherApiError, WeatherCondition } from "../common/WeatherApi";
+import {
+  fetchWeatherCondition,
+  WeatherApiError,
+  WeatherCondition,
+} from "../common/WeatherApi";
+import { Result } from "../Result";
 
 // In this exercise we don't have any Result usage to start with.
 // Use what you've learnt in other exercises and your understanding of andThen to refactor this code.
@@ -9,112 +14,125 @@ import { fetchWeatherCondition, WeatherApiError, WeatherCondition } from "../com
 // Hint. If you find you require variables used in one operation further down the chain,
 // that might mean you are missing an abstraction or a function
 export const getWeatherSummary: RequestHandler = (request) => {
+  return validateRequest(request.query)
+    .andThen(fetchWeatherSummary)
+    .match(createOkResponse, createErrorResponse);
+};
+
+function fetchWeatherConditionResult(
+  weatherRequest: string,
+  city: string
+): Result<WeatherCondition, string> {
   try {
-    const {city, date} = validateRequest(request.query)
-
-    const weatherRequest = createWeatherRequest(city, date)
-
-    try {
-      const weather = fetchWeatherCondition(weatherRequest)
-      return Ok({
-        body: {
-          summary: formatSummary(weather, date, city)
-        }
-      })
-    } catch (error) {
-      return createErrorResponse(error as WeatherApiError, city)
-    }
+    const weather = fetchWeatherCondition(weatherRequest);
+    return Result.ok(weather);
   } catch (error) {
-    return BadRequest({
-      body: {
-        error,
-      }
-    })
+    if (error === "invalidLocation") {
+      return Result.err(`Invalid city name provided ('${city}').`);
+    } else {
+      return Result.err(
+        "Could not handle request at the moment. Please try again later."
+      );
+    }
   }
 }
 
-function validateRequest(body: any) {
-  const { city, time: timeString } = body
+function fetchWeatherSummary(weatherData: WeatherData) {
+  let { city, date } = weatherData;
+  const request = createWeatherRequest(city, date);
+
+  return fetchWeatherConditionResult(request, city).map((weather) =>
+    formatSummary(weather, date, city)
+  );
+}
+
+type WeatherData = { city: string; date: Date };
+function validateRequest(body: any): Result<WeatherData, string> {
+  const { city, time: timeString } = body;
   if (!city) {
-    throw "Request body did not contain a 'city' field"
+    return Result.err("Request body did not contain a 'city' field");
   }
 
   if (!timeString) {
-    throw "Request body did not contain a 'time' field"
+    return Result.err("Request body did not contain a 'time' field");
   }
 
-  if (typeof city !== 'string') {
-    throw "Request body did not contain a valid 'city' field"
+  if (typeof city !== "string") {
+    return Result.err("Request body did not contain a valid 'city' field");
   }
 
-  const time = parseInt(timeString)
+  const time = parseInt(timeString);
   if (isNaN(time)) {
-    throw "Request body did not contain a valid 'time' field"
+    return Result.err("Request body did not contain a valid 'time' field");
   }
 
-  return {
+  return Result.ok({
     city,
     date: new Date(time),
-  }
+  });
 }
 
 function createWeatherRequest(city: string, date: Date) {
-  const d = date.getUTCDate().toString().padStart(2, '0')
-  const m = (date.getUTCMonth() + 1).toString().padStart(2, '0')
-  const y = date.getUTCFullYear()
-  return `${city} @ ${d}/${m}/${y}`
+  const d = date.getUTCDate().toString().padStart(2, "0");
+  const m = (date.getUTCMonth() + 1).toString().padStart(2, "0");
+  const y = date.getUTCFullYear();
+  return `${city} @ ${d}/${m}/${y}`;
 }
 
 function formatSummary(weather: WeatherCondition, date: Date, city: string) {
-  const temperatureSymbol = weather.temperatureScale.at(0)?.toUpperCase()
-  const dateString = formatDateString(date)
-  return `It will be ${weather.condition} (${weather.temperature} ${temperatureSymbol}) in ${city} on the ${dateString}`
+  const temperatureSymbol = weather.temperatureScale.at(0)?.toUpperCase();
+  const dateString = formatDateString(date);
+  return `It will be ${weather.condition} (${weather.temperature} ${temperatureSymbol}) in ${city} on the ${dateString}`;
 }
 
 function formatDateString(date: Date) {
-  return `${dateToString(date.getUTCDate())} of ${MONTHS[date.getUTCMonth()]}`
+  return `${dateToString(date.getUTCDate())} of ${MONTHS[date.getUTCMonth()]}`;
 }
 
 const MONTHS = [
-  'January',
-  'February',
-  'March',
-  'April',
-  'May',
-  'June',
-  'July',
-  'August',
-  'September',
-  'October',
-  'November',
-  'December',
-]
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
 function dateToString(date: number) {
-  return `${date}${getDateSuffix(date)}`
+  return `${date}${getDateSuffix(date)}`;
 }
 
 function getDateSuffix(date: number) {
   switch (date % 10) {
-    case 1: return 'st'
-    case 2: return 'nd'
-    case 3: return 'rd'
-    default: return 'th'
+    case 1:
+      return "st";
+    case 2:
+      return "nd";
+    case 3:
+      return "rd";
+    default:
+      return "th";
   }
 }
 
-function createErrorResponse(error: WeatherApiError, city: string) {
-  if (error === 'invalidLocation') {
-    return BadRequest({
-      body: {
-        error: `Invalid city name provided ('${city}').`,
-      }
-    })
-  }
-
+function createErrorResponse(errorMessage: string) {
   return BadRequest({
     body: {
-      error: 'Could not handle request at the moment. Please try again later.',
-    }
-  })
+      error: errorMessage,
+    },
+  });
+}
+
+function createOkResponse(summary: string) {
+  return Ok({
+    body: {
+      summary,
+    },
+  });
 }
